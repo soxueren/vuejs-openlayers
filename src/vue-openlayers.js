089263847c11b887f29e4e2ec85f4346a8ae3cf7
rukandafaridsi <rukanda@intimap.com>
2017-04-18 08:52:11 +0700

    Refactor

--- src/vue-openlayers.js
+++ src/vue-openlayers.js
@@ -1,201 +1,194 @@
-(function () {
-  var olMap = require('ol/map');
-  var olView = require('ol/view');
-  var olTile = require('ol/layer/tile');
-  var olLayerVector = require('ol/layer/vector');
-  var olSourceVector = require('ol/source/vector');
-  var olOSM = require('ol/source/osm');
-  var olXYZ = require('ol/source/xyz');
-  var olControl = require('ol/control');
-  var olInteraction = require('ol/interaction');
-  var olProj = require('ol/proj');
-  var olFeature = require('ol/feature');
-  var olPoint = require('ol/geom/point');
-  var olStyle = require('ol/style/style');
-  var olIcon = require('ol/style/icon');
+import OlMap from 'ol/map'
+import OlView from 'ol/view'
+import OlTile from 'ol/layer/tile'
+import OlLayerVector from 'ol/layer/vector'
+import OlSourceVector from 'ol/source/vector'
+import OlOSM from 'ol/source/osm'
+import OlXYZ from 'ol/source/xyz'
+import OlControl from 'ol/control'
+import OlInteraction from 'ol/interaction'
+import OlProj from 'ol/proj'
+import OlFeature from 'ol/feature'
+import OlPoint from 'ol/geom/point'
+import OlStyle from 'ol/style/style'
+import OlIcon from 'ol/style/icon'
 
-  var Maps = [];
-  var Views = [];
+export default {
+  install: function (Vue) {
+    this.Maps = []
+    this.Views = []
 
-  var VueOpenlayers = {
-    install: function (Vue) {
-      Vue.prototype.$openlayers = this;
-      Vue.openlayers = this;
-    },
+    Vue.prototype.$openlayers = this
+    Vue.openlayers = this
+  },
 
-    getMaps: function () {
-      return Maps;
-    },
+  getMaps: function () {
+    return this.Maps
+  },
 
-    getMap: function (element) {
-      return Maps[element];
-    },
+  getMap: function (element) {
+    return this.Maps[element]
+  },
 
-    addView: function (setting) {
-      Views[setting.element] = new olView({
-        center: setting.center,
-        zoom: setting.zoom
-      });
+  addView: function (setting) {
+    this.Views[setting.element] = new OlView({
+      center: setting.center,
+      zoom: setting.zoom
+    })
 
-      return Views[setting.element];
-    },
+    return this.Views[setting.element]
+  },
 
-    getView: function (element) {
-      return Views[element];
-    },
+  getView: function (element) {
+    return this.Views[element]
+  },
 
-    getLayers: function (element) {
-      return Maps[element]['layers'];
-    },
+  getLayers: function (element) {
+    return this.Maps[element]['layers']
+  },
 
-    getLayer: function (element, name) {
-      return Maps[element]['layers'][name];
-    },
+  getLayer: function (element, name) {
+    return this.Maps[element]['layers'][name]
+  },
 
-    /* Add New Layer
-    **
-    ** Param
-    ** - setting
-    **   type = Object
-    **   data =
-    **   - element (String)
-    **   - name (String)
-    **   - type (Object String {OSM, XYZ, Vector})
-    **   - url (String) -- If XYZ
-    */
-    addLayer: function (setting) {
-      var layer;
+  /* Add New Layer
+  **
+  ** Param
+  ** - setting
+  **   type = Object
+  **   data =
+  **   - element (String)
+  **   - name (String)
+  **   - type (Object String {OSM, XYZ, Vector})
+  **   - url (String) -- If XYZ
+  */
+  addLayer: function (setting) {
+    var layer
 
-      switch (setting.type) {
-        case 'OSM':
-          layer = new olTile({
-            source: new olOSM()
-          })
-          break;
-        case 'XYZ':
-          layer = new olTile({
-            source: new olXYZ({
-              url: setting.url
-            })
-          })
-          break;
-        case 'Vector':
-          layer = new olLayerVector({
-            source: new olSourceVector({features: []})
-          })
-          break;
-      }
-
-      Maps[setting.element]['layers'][setting.name] = layer;
-      Maps[setting.element].addLayer(Maps[setting.element]['layers'][setting.name]);
-      return Maps[setting.element]['layers'][setting.name];
-    },
-
-    getVisibleLayer: function (element, name) {
-      return Maps[element]['layers'][name].getVisible();
-    },
-
-    setVisibleLayer: function (element, name, isVisible) {
-      Maps[element]['layers'][name].setVisible(isVisible);
-    },
-
-    getExtent: function (element) {
-      return Views[element].calculateExtent(Maps[element].getSize());
-    },
-
-    getZoom: function (element) {
-      return Views[element].getZoom();
-    },
-
-    getCenter: function (element) {
-      return Views[element].getCenter();
-    },
-
-    transform: function (coord) {
-      return Proj.transform(coord, 'EPSG:3857', 'EPSG:4326');
-    },
-
-    transformExtent: function (extent) {
-      return Proj.transformExtent(extent, 'EPSG:3857', 'EPSG:4326');
-    },
-
-    /* Add Marker
-    **
-    ** Param
-    ** - setting
-    **   type = Object
-    **   data =
-    **   - id (String)
-    **   - element (String)
-    **   - layer (String)
-    **   - coord (Array)
-    **   - anchor (Array)
-    **   - icon (String)
-    */
-    addMarker: function (setting) {
-      var geom = new olPoint(olProj.fromLonLat(setting.coord));
-      var feature = new olFeature(geom);
-
-      feature.setStyle([
-        new olStyle({
-          image: new olIcon(({
-            anchor: ((setting.anchor !== undefined) : return setting.anchor ? return [0, 0]),
-            anchorXUnits: 'fraction',
-            anchorYUnits: 'fraction',
-            opacity: 1,
-            src: setting.icon
-          }))
+    switch (setting.type) {
+      case 'OSM':
+        layer = new OlTile({
+          source: new OlOSM()
         })
-      ]);
-
-      feature.setId(setting.id);
-      Maps[setting.element]['layers'][setting.layer].getSource().addFeature(feature);
-    },
-
-    /* Initialize Openlayers Maps
-    **
-    ** Param
-    ** - setting
-    **   type = Object
-    **   data =
-    **   - element (String)
-    **   - center (Array)
-    **   - zoom (Number)
-    **   - enableAttibution (Boolean)
-    **   - enablePan (Boolean)
-    **   - enableZoomButton (Boolean)
-    **   - enableMouseWheelZoom (Boolean)
-    **   - enableDoubleClickZoom (Boolean)
-    */
-    init: function (setting) {
-      this.addView(setting);
-
-      Maps[setting.element] = new olMap({
-        layers: [],
-        controls: olControl.defaults({
-          attribution: (setting.enableAttibution !== undefined && setting.enableAttibution !== false),
-          zoom: (setting.enableZoomButton !== undefined && setting.enableZoomButton !== false)
-        }),
-        target: setting.element,
-        view: Views[setting.element],
-        interactions: olInteraction.defaults({
-          dragPan: (setting.enablePan !== undefined && setting.enablePan !== false),
-          mouseWheelZoom: (setting.enableMouseWheelZoom !== undefined && setting.enableMouseWheelZoom !== false),
-          doubleClickZoom: (setting.enableDoubleClickZoom !== undefined && setting.enableDoubleClickZoom !== false)
+        break
+      case 'XYZ':
+        layer = new OlTile({
+          source: new OlXYZ({
+            url: setting.url
+          })
         })
-      });
-
-      Maps[setting.element]['layers'] = [];
-      return Maps[setting.element];
+        break
+      case 'Vector':
+        layer = new OlLayerVector({
+          source: new OlSourceVector({features: []})
+        })
+        break
     }
-  };
 
-  if (typeof exports == "object") {
-    module.exports = VueOpenlayers;
-  } else if (typeof define == "function" && define.amd) {
-    define([], function(){ return VueOpenlayers; })
-  } else if (window.Vue) {
-    window.VueOpenlayers = VueOpenlayers;
-    Vue.use(VueOpenlayers);
+    this.Maps[setting.element]['layers'][setting.name] = layer
+    this.Maps[setting.element].addLayer(this.Maps[setting.element]['layers'][setting.name])
+    return this.Maps[setting.element]['layers'][setting.name]
+  },
+
+  getVisibleLayer: function (element, name) {
+    return this.Maps[element]['layers'][name].getVisible()
+  },
+
+  setVisibleLayer: function (element, name, isVisible) {
+    this.Maps[element]['layers'][name].setVisible(isVisible)
+  },
+
+  getExtent: function (element) {
+    return this.Views[element].calculateExtent(this.Maps[element].getSize())
+  },
+
+  getZoom: function (element) {
+    return this.Views[element].getZoom()
+  },
+
+  getCenter: function (element) {
+    return this.Views[element].getCenter()
+  },
+
+  transform: function (coord) {
+    return OlProj.transform(coord, 'EPSG:3857', 'EPSG:4326')
+  },
+
+  transformExtent: function (extent) {
+    return OlProj.transformExtent(extent, 'EPSG:3857', 'EPSG:4326')
+  },
+
+  /* Add Marker
+  **
+  ** Param
+  ** - setting
+  **   type = Object
+  **   data =
+  **   - id (String)
+  **   - element (String)
+  **   - layer (String)
+  **   - coord (Array)
+  **   - anchor (Array)
+  **   - icon (String)
+  */
+  addMarker: function (setting) {
+    var geom = new OlPoint(OlProj.fromLonLat(setting.coord))
+    var feature = new OlFeature(geom)
+
+    feature.setStyle([
+      new OlStyle({
+        image: new OlIcon(({
+          anchor: setting.anchor,
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          opacity: 1,
+          src: setting.icon
+        }))
+      })
+    ])
+
+    feature.setId(setting.id)
+    this.Maps[setting.element]['layers'][setting.layer].getSource().addFeature(feature)
+  },
+
+  /* Initialize Openlayers Maps
+  **
+  ** Param
+  ** - setting
+  **   type = Object
+  **   data =
+  **   - element (String)
+  **   - center (Array)
+  **   - zoom (Number)
+  **   - enableAttibution (Boolean)
+  **   - enablePan (Boolean)
+  **   - enableZoomButton (Boolean)
+  **   - enableMouseWheelZoom (Boolean)
+  **   - enableDoubleClickZoom (Boolean)
+  */
+  init: function (setting) {
+    this.addView(setting)
+
+    this.Maps[setting.element] = new OlMap({
+      layers: [
+        new OlTile({
+          source: new OlOSM()
+        })
+      ],
+      controls: OlControl.defaults({
+        attribution: (setting.enableAttibution !== undefined && setting.enableAttibution !== false),
+        zoom: (setting.enableZoomButton !== undefined && setting.enableZoomButton !== false)
+      }),
+      target: setting.element,
+      view: this.Views[setting.element],
+      interactions: OlInteraction.defaults({
+        dragPan: (setting.enablePan !== undefined && setting.enablePan !== false),
+        mouseWheelZoom: (setting.enableMouseWheelZoom !== undefined && setting.enableMouseWheelZoom !== false),
+        doubleClickZoom: (setting.enableDoubleClickZoom !== undefined && setting.enableDoubleClickZoom !== false)
+      })
+    })
+
+    this.Maps[setting.element]['layers'] = []
+    return this.Maps[setting.element]
   }
-})();
+}
